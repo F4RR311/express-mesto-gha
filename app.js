ddab3731cbcd27ab46d07f054c499116d0dd97da
F4RR311 <valery.leo2016@yandex.ru>
2022-06-17 00:03:01 +0300

    fix-auto-test

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const routes = require('./routes/index');
+
+const users = require('./routes/users');
+const cards = require('./routes/cards');
 
 // Слушаем 3000 порт
 const {PORT = 3000} = process.env;
-
 const app = express();
 
+const NOT_FOUND = 404;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {useNewUrlParser: true,});
+
 app.use((req, res, next) => {
     req.user = {
         _id: '62a90c7d51e8680fbf057f48' // вставьте сюда _id созданного в предыдущем пункте пользователя
     };
-
     next();
 });
 
-app.use(routes);
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {useNewUrlParser: true,});
+app.use('/users', users);
+app.use('/cards', cards);
+
+app.use((req, res) => {
+    res.status(NOT_FOUND).send({ message: 'Путь не найден' });
+});
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
