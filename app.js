94e9a8e8b7c22a1ad4def4e9fa7cb6bfe2d375f8
F4RR311 <valery.leo2016@yandex.ru>
2022-06-18 15:34:52 +0300

    fix review

--- app.js
+++ app.js
@@ -3,26 +3,25 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const routerErrorWay = require('./routes/errorsway');
 
-
 // Слушаем 3000 порт
-const {PORT = 3000} = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {useNewUrlParser: true,});
 
 app.use((req, res, next) => {
-    req.user = {
-        _id: '62a90c7d51e8680fbf057f48' // вставьте сюда _id созданного в предыдущем пункте пользователя
-    };
-    next();
+  req.user = {
+    _id: '62a90c7d51e8680fbf057f48' // вставьте сюда _id созданного в предыдущем пункте пользователя
+  };
+  next();
 });
 
 app.use(routerErrorWay);
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
+  console.log(`App listening on port ${PORT}`);
 
 });
